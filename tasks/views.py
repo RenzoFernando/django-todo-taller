894390143813a093968e00d70fd887ed148f6ded
Renzo Fernando <renzofernando.md@gmail.com>
2025-03-11 15:09:05 -0500

    edicion 2

---
+++ tasks/views.py
@@ -0,0 +1,27 @@
+import datetime
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from tasks.models import Task, Status
+
+
+# Create your views here.
+
+#home(request)
+def home(request):
+    return render(request, 'tasks/list.html')
+
+#create_task(request)
+def create_task(request):
+    return render(request, 'tasks/create.html')
+
+#list_tasks(request)
+def list_tasks(request):
+    return render(request, 'tasks/list.html')
+
+#edit_task(request, task_id)
+def edit_task(request, task_id):
+    return render(request, 'tasks/edit.html')
+
+#delete_task(request, task_id)
+def delete_task(request, task_id):
+    return render(request, 'tasks/delete.html')
